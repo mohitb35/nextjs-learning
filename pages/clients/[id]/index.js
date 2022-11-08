dde708fddc3277cabfaacbb45e443a802488b63d
mohitb35 <44917347+mohitb35@users.noreply.github.com>
2022-11-08 19:41:20 +0530

    programatic navigation
    - router.push

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -4,6 +4,16 @@ import { useRouter } from 'next/router';
 function ClientPage() {
 	const router = useRouter();
 
+	function openProject() {
+		router.push({
+			pathname: '/clients/[id]/[clientProjectId]',
+			query: {
+				id: router.query.id,
+				clientProjectId: 'ProjectA',
+			},
+		});
+	}
+
 	return (
 		<div>
 			<h1>The Client Page - {router.query.id}</h1>
@@ -12,6 +22,7 @@ function ClientPage() {
 					<Link href="/clients/max/ProjectX">Project X</Link>
 				</li>
 			</ul>
+			<button onClick={openProject}>Load Project A</button>
 		</div>
 	);
 }
