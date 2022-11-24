c6534133520be65d728dc4b51a7c2705485ff6a3
mohitb35 <44917347+mohitb35@users.noreply.github.com>
2022-11-24 10:49:12 +0530

    starter code

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
-function HomePage() {
-	return <div>Welcome to Next.js!</div>;
+function HomePage(props) {
+	return (
+		<ul>
+			<li>Product 1</li>
+			<li>Product 2</li>
+			<li>Product 3</li>
+		</ul>
+	);
 }
 
 export default HomePage;
