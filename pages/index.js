f84343bbc0f2cb9f1709cc0d21d87a34bc483c18
mohitb35 <44917347+mohitb35@users.noreply.github.com>
2022-11-25 11:01:39 +0530

    e.g. other return properties for getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -13,18 +13,26 @@ function HomePage(props) {
 	);
 }
 
-export async function getStaticProps() {
+export async function getStaticProps(context) {
 	console.log('regenerating');
 	const filePath = path.join(process.cwd(), 'data', 'dummy_backend.json');
 	const jsonData = await fs.readFile(filePath);
 
 	const data = JSON.parse(jsonData);
 
+	if (!data) {
+		return { redirect: { destination: '/not-found' } };
+	}
+
+	if (data.products?.length === 0) return { notFound: true };
+
 	return {
 		props: {
 			products: data.products,
 		},
 		revalidate: 10, //Specify value in seconds after which the page should be rerendered (ISR)
+		// notFound: true, //could be used to render 404 page some times because data could not be fetched
+		// redirect //specify redirect path (could be conditional)
 	};
 }
 
