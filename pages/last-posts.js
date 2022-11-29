2208cbca987b61ca66dbaf26ed283fdcc924b4bf
mohitb35 <44917347+mohitb35@users.noreply.github.com>
2022-11-29 12:01:03 +0530

    use prefetching with csr
    - e.g. pagination

--- pages/last-posts.js
+++ pages/last-posts.js
@@ -1,51 +1,63 @@
 import { useEffect, useState } from 'react';
 import useSWR from 'swr';
 
-function LastPostsPage() {
-	/* const [isLoading, setIsLoading] = useState(false); */
-	const [posts, setPosts] = useState(null);
+function LastPostsPage(props) {
+	const [posts, setPosts] = useState(props.posts);
+	const [page, setPage] = useState(Number(props.page));
 
 	const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 	const { data, error } = useSWR(
-		'https://jsonplaceholder.typicode.com/posts?_page=1',
+		`https://jsonplaceholder.typicode.com/posts?_page=${page}`,
 		fetcher
 	);
 
 	useEffect(() => {
 		if (data) {
-			console.log(data);
 			setPosts(data);
 		}
 	}, [data]);
 
-	/* useEffect(() => {
-		setIsLoading(true);
-		fetch('https://jsonplaceholder.typicode.com/posts?_page=1')
-			.then((res) => res.json())
-			.then((data) => {
-				setPosts(data);
-				setIsLoading(false);
-			});
-	}, []); */
+	const fetchPrevious = () => {
+		setPage((page) => page - 1);
+	};
+
+	const fetchNext = () => {
+		setPage((page) => page + 1);
+	};
 
 	if (error) {
 		return <p>No data fetched</p>;
 	}
 
-	if (!data || !posts) {
+	if (!data && !posts) {
 		return <p>Loading..</p>;
 	}
 
 	return (
 		posts && (
-			<ul>
-				{posts.map((post) => (
-					<li key={post.id}>{post.title}</li>
-				))}
-			</ul>
+			<>
+				<ul>
+					{posts.map((post) => (
+						<li key={post.id}>{post.title}</li>
+					))}
+				</ul>
+				{page !== 1 && <button onClick={fetchPrevious}>Previous</button>}
+				<button onClick={fetchNext}>Next</button>
+			</>
 		)
 	);
 }
 
 export default LastPostsPage;
+
+export async function getStaticProps() {
+	const response = await fetch(
+		'https://jsonplaceholder.typicode.com/posts?_page=1'
+	);
+	const data = await response.json();
+
+	return {
+		props: { posts: data, page: 1 },
+	};
+}
